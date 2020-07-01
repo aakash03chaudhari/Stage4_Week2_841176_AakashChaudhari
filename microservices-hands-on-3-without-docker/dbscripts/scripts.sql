26dc5c7aa78ee25040f931c4daa4e4cd0370401b
aakash03chaudhari <aakash03chaudhari@gmail.com>
2020-07-01 23:25:36 +0530

    Issue Solved

--- microservices-hands-on-3-without-docker/dbscripts/scripts.sql
+++ microservices-hands-on-3-without-docker/dbscripts/scripts.sql
@@ -3,6 +3,6 @@ use employee_database;
 
 create table if not exists  user (id int, active bit(1) , password varchar(255), roles varchar(255), user_name varchar(255) );
 
-insert into book values(1, b'1', "admin", "ROLE_ADMIN", "admin");
+insert into user values(1, b'1', "admin", "ROLE_ADMIN", "admin");
 
 create table if not exists  employee (emp_id int, date_of_birth datetime , name varchar(30), permanent bit(1), salary double);
