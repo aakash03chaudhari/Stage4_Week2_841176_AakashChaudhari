5b187c73b72200151c3ff6e31e3403e14b3c20aa
aakash03chaudhari <aakash03chaudhari@gmail.com>
2020-07-06 00:34:36 +0530

    Spring Cloud Config Server add in Practice and Final Check

--- microservices-practice-final-check/zuul-gateway/src/main/java/com/cognizant/zuulgateway/ZuulGatewayApplication.java
+++ microservices-practice-final-check/zuul-gateway/src/main/java/com/cognizant/zuulgateway/ZuulGatewayApplication.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -20,6 +21,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableDiscoveryClient
 @EnableZuulProxy
 @EnableSwagger2
+
 public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
